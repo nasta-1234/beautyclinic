8062ef6169db115d55707ab42fb149b862ea8e85
sabiranajwa <sabiranajwa76@gmail.com>
2025-10-05 18:55:58 +0700

    update folder beautyclinic

--- components/admin_footer.php
+++ components/admin_footer.php
@@ -7,11 +7,62 @@
             <button type="submit" class="btn">Berlangganan</button>
         </div>
         <div class="icons">
-            <i class="bx-bxl-facebook"></i>
-            <i class="bx-bxl-instagram"></i>
-            <i class="bx-bxl-linkedin"></i>
-            <i class="bx-bxl-twiter"></i>
-            <i class="bx-bxl-pinterest-alt"></i>
+            <i class="bx bxl-facebook"></i>
+            <i class="bx bxl-instagram"></i>
+            <i class="bx bxl-linkedin"></i>
+            <i class="bx bxl-twitter"></i>
+            <i class="bx bxl-pinterest-alt"></i>
         </div>
     </div>
 </div>
+<div class="client">
+    <div class="img-box">
+        <img src="../image/client.png">
+    </div>
+    <div class="img-box">
+        <img src="../image/client0.png">
+    </div>
+    <div class="img-box">
+        <img src="../image/client1.png">
+    </div>
+    <div class="img-box">
+        <img src="../image/client2.png">
+    </div>
+    <div class="img-box">
+        <img src="../image/client3.png">
+    </div>
+    <div class="img-box">
+        <img src="../image/client4.png">
+    </div>
+    </div>
+</div>
+<footer>
+    <div class="content">
+        <div class="box">
+            <img src="../image/logo.png">
+        <p>Satu dashboard untuk mempermudah, mempercantik, dan mengembangkan layanan Anda</p>
+        <a href="contact.php" class="btn">Contact Us</a>
+        </div>
+        <div class="box">
+            <h3>Akun Saya</h3>
+            <a href=""><i class="bx bx-chevron-right">Akun Saya</i></a>
+            <a href=""><i class="bx bx-chevron-right">Riwayat Pesanan</i></a>
+            <a href=""><i class="bx bx-chevron-right">Karyawan</i></a>
+            <a href=""><i class="bx bx-chevron-right">Bulletin</i></a>
+        </div>
+        <div class="box">
+            <h3>Informasi</h3>
+            <a href=""><i class="bx bx-chevron-right">Tentang Kami</i></a>
+            <a href=""><i class="bx bx-chevron-right">Riwayat Layanan</i></a>
+            <a href=""><i class="bx bx-chevron-right">Kebijakan Privasi</i></a>
+            <a href=""><i class="bx bx-chevron-right">Syarat $ Ketentuan</i></a>
+        </div>
+        <div class="box">
+            <h3>Tambahan</h3>
+            <a href=""><i class="bx bx-chevron-right">Merek</i></a>
+            <a href=""><i class="bx bx-chevron-right">Riwayat Pesanan</i></a>
+            <a href=""><i class="bx bx-chevron-right">Karyawan</i></a>
+            <a href=""><i class="bx bx-chevron-right">Bulletin</i></a>
+        </div>
+    </div>
+</footer>
