9733572494a05253565370838923b2ea2f8c8470
sabiranajwa <sabiranajwa76@gmail.com>
2025-10-13 23:51:25 +0700

    menambahkan layanan

--- admin/add_services.php
+++ admin/add_services.php
@@ -36,24 +36,76 @@
 
         $status ='active';
 
-        $select_image = $conn->prepare("SELECT * FROM layanan WHERE image = ?");
-        $select_image->execute([$image]);
+        $select_image = $conn->prepare("SELECT * FROM layanan WHERE foto = ?");
+        $select_image->execute([$foto]);
 
-        if (isset($image)) {
+        if (isset($foto)) {
             if ($select_image->rowCount() > 0) {
                 $warning_msg[] = 'image name repeated';
             }elseif ($image_size > 2000000) {
                 $warning_msg[] = 'image size is too large';
             }else {
-                move_uploaded_file($image_tmp_name, $image_folder)
+                move_uploaded_file($image_tmp_name, $image_folder);
             }
         }else {
-            $image = '';
+            $foto = '';
         }
-        if($select_image->rowCount() > 0 AND $iamge != ''){
+        if($select_image->rowCount() > 0 AND $foto != ''){
             $warning_msg[] = 'plese rename your image';
         }else {
             $insert_services = $conn->prepare("INSERT INTO layanan (id, nama, harga, foto, detail_layanan, kategori, status) VALUES(?,?,?,?,?,?,?)");
+            $insert_services->execute([$id,$nama, $harga, $rename, $detail_layanan, $kategori, $status]);
+            $success_msg[] = 'Pelayanan Berhasil Ditambahkan!!';
+        }
+    }
+    //adding service in database as draft
+    if (isset($_POST['draft'])) {
+        
+        $id = unique_id();
+
+        $nama = $_POST['nama'];
+        $nama = filter_var($nama, FILTER_SANITIZE_STRING);
+
+        $harga = $_POST['harga'];
+        $harga = filter_var($harga, FILTER_SANITIZE_STRING);
+
+        $detail_layanan = $_POST['detail_layanan'];
+        $detail_layanan = filter_var($detail_layanan, FILTER_SANITIZE_STRING);
+
+        $kategori = $_POST['kategori'];
+        $kategori = filter_var($kategori, FILTER_SANITIZE_STRING);
+        
+        
+        $foto = $_FILES['foto']['name'];
+        $foto = filter_var($foto, FILTER_SANITIZE_STRING);
+        $ext = pathinfo($foto, PATHINFO_EXTENSION);
+        $rename = unique_id().'.'.$ext;
+        $image_size = $_FILES['foto']['size'];
+        $image_tmp_name = $_FILES['foto']['tmp_name'];
+        $image_folder = '../uploaded_files/'.$rename;
+
+        $status ='deactive';
+
+        $select_image = $conn->prepare("SELECT * FROM layanan WHERE foto = ?");
+        $select_image->execute([$foto]);
+
+        if (isset($foto)) {
+            if ($select_image->rowCount() > 0) {
+                $warning_msg[] = 'image name repeated';
+            }elseif ($image_size > 2000000) {
+                $warning_msg[] = 'image size is too large';
+            }else {
+                move_uploaded_file($image_tmp_name, $image_folder);
+            }
+        }else {
+            $foto = '';
+        }
+        if($select_image->rowCount() > 0 AND $foto != ''){
+            $warning_msg[] = 'plese rename your image';
+        }else {
+            $insert_services = $conn->prepare("INSERT INTO layanan (id, nama, harga, foto, detail_layanan, kategori, status) VALUES(?,?,?,?,?,?,?)");
+            $insert_services->execute([$id,$nama, $harga, $rename, $detail_layanan, $kategori, $status]);
+            $success_msg[] = 'Pelayanan Berhasil Disimpan Didraf!!';
         }
     }
 ?>
@@ -90,18 +142,18 @@
             <img src="../image/layer.jpg" width="100">
         </div>
         <div class="form-container">
-            <form action="" methode="post" enctype="multipart/form-data" class="register">
+            <form action="" method="post" enctype="multipart/form-data" class="register">
             <div class="input-field">
                 <p>Nama Layanan <span>*</span></p>
-                <input type="text" name="nama" placeholder="add service name" required class="box">
+                <input type="text" name="nama" placeholder="tambahkan nama layanan" required class="box">
             </div>
             <div class="input-field">
                 <p>Harga Layanan <span>*</span></p>
-                <input type="number" name="price" placeholder="add service price" required class="box">
+                <input type="number" name="harga" placeholder="tambahkan harga layanan" required class="box">
             </div>
             <div class="input-field">
                 <p>Detail Layanan <span>*</span></p>
-                <textarea name="detail_layanan"required placeholder="sevice description"class="box"></textarea>
+                <textarea name="detail_layanan"required placeholder="penjelasan tentang layanan"class="box"></textarea>
             </div>
             <div class="input-field">
                 <p>Kategori Layanan <span>*</span></p>
@@ -118,7 +170,10 @@
                 <p>Layanan image <span>*</span></p>
                 <input type="file" name="foto" accept="image/*" required class="box">
             </div>
-            <button type="submit" name="add_service" class="btn">Tambahkan Layanan</button>
+            <div class="flex-btn">
+                <button type="submit" name="add_service" class="btn">Tambahkan Layanan</button>
+                <button type="submit" name="draft" class="btn">Tambahkan ke Draft</button>
+            </div>
             </form>
         </div>
 
