1dd7b7431505440b6d8baab0c7629c72cccf838e
Zelia-24 <alfirestizelia@gmail.com>
2025-10-29 17:48:17 +0700

    header

--- components/admin_header.php
+++ components/admin_header.php
@@ -8,8 +8,42 @@
 </div>
 <div class="profile-detail">
 <?php  
-$se
+$select_profile = $conn->prepare("SELECT * FROM admin WHERE id = ?");
+$select_profile->execute([$admin_id]);
+
+if ($select_profile->rowCount() > 0){
+    $fetch_profile = $select_profile->fetch(PDO::FETCH_ASSOC);
+}
+?>
+<div class="profile">
+  <img src="../uploaded_files/<?= $fetch_profile['image']; ?>" class="logo-img">
+  <p> <?= $fetch_profile['nama']; ?></p>
+</div>
+<div class="flex-btn">
+  <a href="profile.php" class="btn"> profile </a>
+  <a href="../components/admin_logout.php" onclick="return confirm('logout from this website');" class="btn">logout</a>
+</div>
+<?php
 
 ?>
 </div>
 </header>
+
+<div class="sidebar">
+  <div class="profile">
+    <img src="../uploaded_files/<?= $fetch_profile['image']; ?>" class="logo-img">
+    <p> <?= $fetch_profile['nama']; ?></p>
+</div>
+<h5> menu </h5>
+<div class="navbar">
+  <ul>
+    <li> <a href="dashbord.php"><i class="bx bxs-home-smile"></i>dashboard</a></li>
+    <li> <a href="add_services.php"></a><i class="bx bxs-shooping-bags"></i>add services</a></li>
+    <li> <a href="view_services.php"><i class="bx bxs-food-menu"></i>view services</a></li>
+    <li> <a href="add_employee.php"></a><i class="bx bxs-shooping-bags"></i>add employee</a></li>
+    <li> <a href="view_employee.php"></a><i class="bx bxs-food-menu"></i>view employee</a></li>
+    <li> <a href="user_account.php"></a><i class="bx bxs-user-detail"></i>account</a></li>
+    <li> <a href="../components/admin_logout.php" onclick="return confirm('logout from this website');"></a><i class="bx bxs-log-out"></i>logout</a></li>
+</ul>
+</div>
+</div>
